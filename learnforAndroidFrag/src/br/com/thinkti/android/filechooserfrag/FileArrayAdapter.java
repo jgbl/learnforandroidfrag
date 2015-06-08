ab7ca76a6cca5e45b85ff30c5a205b2d17df1894
jhmgbl <jhmgbl@gmail.com>
2015-06-08 16:45:23 +0200

    File header added

--- learnforAndroidFrag/src/br/com/thinkti/android/filechooserfrag/FileArrayAdapter.java
+++ learnforAndroidFrag/src/br/com/thinkti/android/filechooserfrag/FileArrayAdapter.java
@@ -1,3 +1,30 @@
+/*
+ * Original at https://code.google.com/p/android-file-chooser/
+ *
+ * Modified by J.M.Goebel
+ *
+ * License:
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Copyright (c) 2015 GPL by J.M.Goebel. All rights reserved.
+ * 
+ * 08.06.2015
+ * 
+ * This file is part of learnforandroid.
+ *
+ * learnforandroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  learnforandroid is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package br.com.thinkti.android.filechooserfrag;
 
 import java.util.List;
